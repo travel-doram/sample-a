e7c1ef8968854ec71dbb75c17fd005413fc443c4
Doram Greenblat <doram@travelstart.com>
2025-04-14 23:09:46 +0200

    Go deploy github with extra gcloud components

--- ci/auth/pre-up.sh
+++ ci/auth/pre-up.sh
@@ -17,7 +17,10 @@ source ${ENVFILE}
 set +o allexport
 
 authKubeGcloud () {
-    gcloud components install gke-gcloud-auth-plugin
+    if ! [ -z "$GITHUB_RUN_ID" ];
+    then 
+        sudo apt-get install google-cloud-cli-gke-gcloud-auth-plugin
+    fi
     gcloud auth configure-docker europe-west4-docker.pkg.dev --quiet
     gcloud container clusters get-credentials ${K8S_CLUSTER} \
         --zone ${K8S_ZONE} --project ${K8S_PROJECT}
